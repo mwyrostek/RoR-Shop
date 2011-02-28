c996d92572d9a5e04078c54837fbdd1d8c115ffb
unknown <C:\Program Files (x86)\The Bat!\Mail>
2011-02-28 19:42:27 +1000

    Added some defence

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Shop::Application.config.secret_token = 'f82d579a33dcb876704205bf69b4c532cb1674d04782cd83e1e10ea06d500612ea4d14fbcafcf11bb7df77b20bdc0617aa65114b6253e7e3efe29cb74d5fe40e'
